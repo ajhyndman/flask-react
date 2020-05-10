fbafdabcfbd40bc9a919abeb0f8339333b90c7ef
Andrew Hyndman <ajhyndman@hotmail.com>
2020-05-10 00:51:54 -0700

    In development, proxy all requests to weback-dev-server

--- server/server/app.py
+++ server/server/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, Response
+from requests import get
+from os import environ
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 
-@app.route('/')
-def hello():
-    return 'hello'
+WEBPACK_DEV_SERVER = 'http://localhost:3000/'
 
-def main():
-  app.run()
+def proxy(path):
+    response = get(f'{WEBPACK_DEV_SERVER}{path}')
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in response.raw.headers.items() if name.lower() not in excluded_headers]
+    return Response(response.content, response.status_code, headers)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET'])
+def reactApp(path):
+    if environ['FLASK_ENV'] == 'development':
+        return proxy(request.path)
+    else:
+        # return static dir
+        pass
