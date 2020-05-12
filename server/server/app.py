03970bd19a215c08ec332e4d79410182b64b98d3
Andrew Hyndman <ajhyndman@hotmail.com>
2020-05-11 20:20:48 -0700

    Use dictionary instead of list comprehension

--- server/server/app.py
+++ server/server/app.py
@@ -17,11 +17,11 @@ def proxy(path):
         "transfer-encoding",
         "connection",
     ]
-    headers = [
-        (name, value)
-        for (name, value) in response.raw.headers.items()
+    headers = {
+        name: value
+        for name, value in response.raw.headers.items()
         if name.lower() not in excluded_headers
-    ]
+    }
     return (response.content, response.status_code, headers)
 
 
