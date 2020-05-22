5e3692608d4f21aaffbef5e812fc28a32a53bd9d
Andrew Hyndman <ajhyndman@hotmail.com>
2020-05-21 19:31:25 -0700

    Serve React Application from Webpack Dev Server

--- server/server/app.py
+++ server/server/app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from os import environ
+
+from flask import Flask, request
+from requests import get
+
+IS_DEV = environ["FLASK_ENV"] == "development"
+WEBPACK_DEV_SERVER_HOST = "http://localhost:3000"
 
 app = Flask(__name__)
+
+
+def proxy(host, path):
+    response = get(f"{host}{path}")
+    excluded_headers = [
+        "content-encoding",
+        "content-length",
+        "transfer-encoding",
+        "connection",
+    ]
+    headers = {
+        name: value
+        for name, value in response.raw.headers.items()
+        if name.lower() not in excluded_headers
+    }
+    return (response.content, response.status_code, headers)
 
 
 @app.route("/")
@@ -11,4 +33,6 @@ def getRoot():
 @app.route("/app/", defaults={"path": "index.html"})
 @app.route("/app/<path:path>")
 def getApp(path):
+    if IS_DEV:
+        return proxy(WEBPACK_DEV_SERVER_HOST, request.path)
     return app.send_static_file(path)
