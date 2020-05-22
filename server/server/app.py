824399f62e936231140356ec549c047bc335c956
Andrew Hyndman <ajhyndman@hotmail.com>
2020-05-21 19:25:17 -0700

    Serve React application from Flask

--- server/server/app.py
+++ server/server/app.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def getRoot():
     return "Welcome!"
+
+
+@app.route("/app/", defaults={"path": "index.html"})
+@app.route("/app/<path:path>")
+def getApp(path):
+    return app.send_static_file(path)
